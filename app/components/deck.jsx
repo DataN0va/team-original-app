e51e55cdd6d3828e7a3b9ee0ed1ea27f4ee6fd1f
aniosu <ameniosu@au.com>
2024-12-29 22:55:53 +0900

    更新

---
+++ app/components/deck.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const deck = () => {
+  return <div>deck</div>;
+};
+
+export default deck;
--- app/routes/fightP1.jsx
+++ app/routes/fightP1.jsx
@@ -1,31 +1,9 @@
 import { useState } from "react";
+import 
+export default function Fightpage() {
 
-export default function fightpage() {
-  const [nowPlayer, setNowPlayer] = useState(true);
-  const [yourState, setYourState] = useState(false);
-  function useCard() {}
-  return (
-    <>
-      <div id="nowAttackerPlayerContainer">
-        <div id="playerName">プレイヤー１</div>
-        <div id="playerACard"></div>
-      </div>
-      <div id="nowDefenderPlayerContainer">
-        <div id="playerName">プレイヤー2</div>
-        <div id="playerDCard"></div>
-      </div>
-      <div id="deck">
-        あなたのカード
-        <button className="cards" id="card1" onClick={() => useCard()}>
-          1
-        </button>
-        <button className="cards" id="card2" onClick={() => useCard()}>
-          2
-        </button>
-        <button className="cards" id="card3" onClick={() => useCard()}>
-          3
-        </button>
-      </div>
-    </>
-  );
+return(
+  <>
+  <</>
+)
 }
