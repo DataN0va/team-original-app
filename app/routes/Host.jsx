7fddaa9bf64a7c09c28660062f3a034056e75999
yuto-suto <uto2004@docomo.ne.jp>
2024-12-29 17:35:30 +0900

    一通り作ってみました

--- app/routes/Host.jsx
+++ app/routes/Host.jsx
@@ -1,10 +1,15 @@
-import React from "react";
+import { useState } from "react";
 import { useNavigate } from "@remix-run/react";
 import { name } from "./top.jsx";
+import { pass } from "./Rooms.jsx";
 import { collection, addDoc, getDocs } from "firebase/firestore";
 import { db } from "../firebase.js";
 
 const Host = () => {
+  const navigate = useNavigate();
+  if (pass.players.length <= 2) {
+    navigate;
+  }
   return (
     <div>
       <h1>対戦相手を探しています・・・</h1>
--- app/routes/Rooms.jsx
+++ app/routes/Rooms.jsx
@@ -1,8 +1,15 @@
 import { useState } from "react";
 import { useNavigate } from "@remix-run/react";
 import { name } from "./top.jsx";
-import { collection, addDoc, getDocs } from "firebase/firestore";
 import { db } from "../firebase.js";
+import {
+  collection,
+  addDoc,
+  getDocs,
+  updateDoc,
+  arrayUnion,
+  doc,
+} from "firebase/firestore";
 
 function Rooms() {
   const [value1, setValue1] = useState("");
@@ -14,6 +21,7 @@ function Rooms() {
     const pass = {
       user: { name },
       password: { value1 },
+      players: [name],
     };
     addDoc(collection(db, "passes"), pass).then((ref) => {
       const newPasses = [...passes];
@@ -29,8 +37,10 @@ function Rooms() {
   function handleClickEntrant() {
     getDocs(collection(db, "passes"));
     for (let i = 0; i < passes.length; i++) {
-      if (value2 == passes[i]) {
-        navigate("/routes/fight2");
+      if (value2 == passes[i].password) {
+        updateDoc(doc(db, "passes"), {
+          players: arrayUnion(name),
+        });
       }
     }
   }
