374cb0978f1cd947edba9ed892c4f352660d47f1
yuto-suto <uto2004@docomo.ne.jp>
2024-12-29 17:43:18 +0900

    通りってつ修正加えました

--- app/routes/Host.jsx
+++ app/routes/Host.jsx
@@ -1,19 +1,17 @@
-import { useState } from "react";
 import { useNavigate } from "@remix-run/react";
-import { name } from "./top.jsx";
 import { pass } from "./Rooms.jsx";
-import { collection, addDoc, getDocs } from "firebase/firestore";
-import { db } from "../firebase.js";
 
 const Host = () => {
   const navigate = useNavigate();
-  if (pass.players.length <= 2) {
-    navigate;
+  if (pass.players.length == 2) {
+    navigate("./fight1.jsx");
   }
   return (
-    <div>
-      <h1>対戦相手を探しています・・・</h1>
-    </div>
+    <>
+      <div>
+        <h1>対戦相手を探しています・・・</h1>
+      </div>
+    </>
   );
 };
 
