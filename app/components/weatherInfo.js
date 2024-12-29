a7280ebac1dedff1eded9fbddc74a46b45aff2f4
riku-2004 <lubii2004@gmail.com>
2024-12-29 23:59:34 +0900

    change weatherInfo.js

--- app/components/weatherInfo.js
+++ app/components/weatherInfo.js
@@ -1,8 +1,13 @@
-import Deck from "../components/deck";
+import Deck from "../components/deck.js";
 const apiKey = "f06f726cf5190bcb1df57391ed6fd1d6";
-const cityList = [1850147 /*Tokyo*/, 6616851];
+const cityList = [
+  1850147 /*JP*/, 2988507 /*FR*/, 4138106 /*US*/, 6094817 /*CA*/,
+  3530597 /*MX*/, 3469058 /*BR*/, 3435910 /*AR*/, 3688689 /*BR*/,
+  3871336 /*CH*/,
+];
 const urlHead = `https://api.openweathermap.org/data/2.5/weather?id=`;
 
+let deckArray = Deck();
 for (let i = 0; i < cityList.length; i++) {
   const url = urlHead + cityList[i] + `&appid=` + apiKey;
   fetch(url)
@@ -10,10 +15,12 @@ for (let i = 0; i < cityList.length; i++) {
       return res.json();
     })
     .then((data) => {
-      // console.log(data);
-      // console.log(data.weather[0].main);
-      Deck[0].name = data.sys.country;
-      Deck[0].weather = data.weather[0].main;
-      console.log(Deck);
+      console.log(data);
+      deckArray[i].temp = data.main.temp;
+      deckArray[i].name = data.sys.country;
+      deckArray[i].weather = data.weather[0].main;
+      console.log(deckArray);
     });
 }
+
+export default deckArray;
