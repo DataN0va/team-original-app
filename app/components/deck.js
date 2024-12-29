754514cfcb3385b75cf50d56bbcc6b29d9b1db99
riku-2004 <lubii2004@gmail.com>
2024-12-29 21:48:14 +0900

    ちょっとだけいじりました、まだAPIの情報を取得できていません

--- app/components/deck.js
+++ app/components/deck.js
@@ -1,6 +1,6 @@
 export default function Deck() {
   const Deck = [
-    { name: "Swords", attack: 10, defence: 0, id: 1 },
+    { name: "Japan", attack: 10, defence: 0, id: 1, weather: "rain" },
     { name: "Shields", attack: 0, defence: 10, id: 2 },
   ];
   return Deck;
--- app/components/weatherInfo.js
+++ app/components/weatherInfo.js
@@ -1,3 +1,4 @@
+import Deck from "../components/deck";
 const apiKey = "f06f726cf5190bcb1df57391ed6fd1d6";
 const cityList = [1850147 /*Tokyo*/, 6616851];
 const urlHead = `https://api.openweathermap.org/data/2.5/weather?id=`;
@@ -9,6 +10,10 @@ for (let i = 0; i < cityList.length; i++) {
       return res.json();
     })
     .then((data) => {
-      console.log(data);
+      // console.log(data);
+      // console.log(data.weather[0].main);
+      Deck[0].name = data.sys.country;
+      Deck[0].weather = data.weather[0].main;
+      console.log(Deck);
     });
 }
