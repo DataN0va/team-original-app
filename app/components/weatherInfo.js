d400b83b9d9ef0c12c0ea06a0aa3ebe29efa817b
riku-2004 <lubii2004@gmail.com>
2024-12-30 10:05:51 +0900

    deckArray作り終わりました

--- app/components/weatherInfo.js
+++ app/components/weatherInfo.js
@@ -1,10 +1,22 @@
 import Deck from "../components/deck.js";
 const apiKey = "f06f726cf5190bcb1df57391ed6fd1d6";
 const cityList = [
-  1850147 /*JP*/, 2988507 /*FR*/, 4138106 /*US*/, 6094817 /*CA*/,
-  3530597 /*MX*/, 3469058 /*BR*/, 3435910 /*AR*/, 3688689 /*BR*/,
-  3871336 /*CH*/,
+  1850147 /*JP*/, 2988507 /*FRフランス*/, 4138106 /*US*/, 6094817 /*CAカナダ*/,
+  3530597 /*MX*/, 3469058 /*BR*/, 3435910 /*ARアルゼンチン*/,
+  3688689 /*COコロンビア*/, 3871336 /*CLチリ*/, 2643743 /*GBイギリス*/,
+  7290255 /*DEドイツ*/, 3169070 /*ITイタリア*/, 6359304 /*ESスペイン*/,
+  524901 /*RUロシア*/, 2673730 /*SEスウェーデン*/, 756135 /*PLポーランド*/,
+  360630 /*EGエジプト*/, 1816670 /*CN中国*/, 2352778 /*NGナイジェリア*/,
+  184745 /*KEケニア*/, 2538475 /*MAモロッコ*/, 1261481 /*INインド*/,
+  1835848 /*KR韓国*/, 108410 /*SAサウジアラビア*/, 1609350 /*THタイ*/,
+  323786 /*TRトルコ*/, 2172517 /*AUオーストラリア*/,
+  2179537 /*NZニュージーランド*/, 281184 /*ILイスラエル*/, 112931 /*IRイラン*/,
+  292968 /*AEアラブ首長国連邦*/, 1880251 /*SGシンガポール*/,
+  1733046 /*MYマレーシア*/, 1176615 /*PKパキスタン*/, 1701668 /*PHフィリピン*/,
+  1337178 /*BDバングラデシュ*/, 2759794 /*NLオランダ*/, 264371 /*GRギリシャ*/,
+  1642911 /*IDインドネシア*/, 3652462 /*ECエクアドル*/,
 ];
+// const toJapaneseList = [{ JP: "日本", FR: "フランス", US: "アメリカ" }];
 const urlHead = `https://api.openweathermap.org/data/2.5/weather?id=`;
 
 let deckArray = Deck();
@@ -15,12 +27,12 @@ for (let i = 0; i < cityList.length; i++) {
       return res.json();
     })
     .then((data) => {
-      console.log(data);
+      // console.log(data);
       deckArray[i].temp = data.main.temp;
       deckArray[i].name = data.sys.country;
       deckArray[i].weather = data.weather[0].main;
-      console.log(deckArray);
+      console.log(deckArray[i]);
     });
 }
-
+console.log(cityList.length);
 export default deckArray;
